5650289bc294832ef7fb89798ad8429000013e38
Kinte <johneskobar14@gmail.com>
2026-09-13 07:15:39 +0100

    Add calculator project

---
+++ calculator.py
@@ -0,0 +1,46 @@
+while True:
+
+    while True:
+        try:
+            user_input = float(input("DO YOUR CALCULATION: "))
+            break
+        except ValueError:
+            print("INVALID INPUT: TRY AGAIN")
+
+    while True:
+        user_operator = input("SELECT OPERATION: '+', '-', '*', '/':  ")
+        if user_operator in ["+", "-", "*", "/"]:
+            break
+        else:
+            print("INPUT A VALID OPERATION TRY AGAIN")
+
+    while True:
+        try:
+            user_input2 = float(input("DO YOUR CALCULATION: "))
+            break
+        except ValueError:
+            print("INVALID INPUT: TRY AGAIN")
+            
+    try:
+        if user_operator == "+":
+            result = user_input + user_input2
+            print(result)
+        elif user_operator == "-":
+            result = user_input - user_input2
+            print(result)
+        elif user_operator == "*":
+            result = user_input * user_input2
+            print(result)
+        elif user_operator == "/":
+            result = user_input / user_input2
+            print(result)
+    except ZeroDivisionError:
+        print("CANNOT DIVIDE BY ZERO")
+
+option1 = "no"
+option2 = "yes"
+calculate_again = input("DO YOU WANT ANOTHER CALCULATION? (yes/no): ")
+if calculate_again != option1, option2:
+    print("INVALID")
+    break
+        
